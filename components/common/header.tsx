5b1b4989f5708f17ca425eadb004943a8d83d7f5
thiennn-fabbi <thiennn@fabbi.com.vn>
2022-01-17 17:32:36 +0700

    todos

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -4,7 +4,7 @@ export interface HeaderProps {
 }
 
 export default function Header (props: HeaderProps) {
-    console.log("Trang header");
+    // console.log("Trang header");
     
   return (
     <div className='bg-gradient-to-b from-blue-600 to-cyan-400 text-center py-10 text-5xl'>
--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Header from '../../components/common/header';
-import dynamic from 'next/dynamic';
 import { HeaderComponent } from '../products/[productId]';
 import { useRouter } from 'next/router';
 export interface TodosProps {}
@@ -8,28 +6,39 @@ export interface TodosProps {}
 export default function Todos(props: TodosProps) {
   const router = useRouter();
   const [todoList, setTodoList] = React.useState([]);
+  const page = router.query?.page || 1
+  console.log(page);
+  
   React.useEffect(() => {
     (async () => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=1`);
+      if(!page) return;
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${page}`);
       const data = await response.json();
       setTodoList(data);
     })();
   }, []);
+  const handleNextPage = () => {
+    router.push(
+      {
+        pathname : '/todos',
+      }
+    )
+  }
   return (
     <div>
       <HeaderComponent />
       <div className="py-5">Query Router : {JSON.stringify(router.query)} </div>
-      <ul className="px-10">
+      <ul className="px-10 py-10">
         {todoList.map((todo: any) => (
           <li 
           className="py-1 text-red-500 cursor-pointer hover:text-red-800 transition-all duration-150" 
           key={todo.id}
-
           >
             {todo.title}
           </li>
         ))}
       </ul>
+      <button className='' onClick={handleNextPage}> Next Page</button>
     </div>
   );
 }
